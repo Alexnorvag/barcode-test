d7d713b3f55d9a7d7b517c9ecc64b8471466c5cc
Alexnorvag <norvag@mail.ru>
2019-11-19 13:30:51 +0300

    Updates

---
+++ precache-manifest.cea551a255735d622255e5f696761289.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a80f28aa33c3d55444174fba39e812a",
+    "url": "/barcode-test/index.html"
+  },
+  {
+    "revision": "a214660c03f1fc025e76",
+    "url": "/barcode-test/static/css/main.12088e9e.chunk.css"
+  },
+  {
+    "revision": "c7994f3fc3de3d7fd925",
+    "url": "/barcode-test/static/js/2.51f93243.chunk.js"
+  },
+  {
+    "revision": "a214660c03f1fc025e76",
+    "url": "/barcode-test/static/js/main.64546672.chunk.js"
+  },
+  {
+    "revision": "4b8c19b10c414c0d14ba",
+    "url": "/barcode-test/static/js/runtime-main.066b2ebe.js"
+  }
+]);
